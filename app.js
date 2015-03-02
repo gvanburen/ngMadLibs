a8a17dfaf9fdb1932f5a6cf23caa99b1e3ed2a05
Greg Van Buren <g.vanburen@gmail.com>
2015-03-01 19:13:58 -0800

    Added controller

---
+++ app.js
@@ -0,0 +1,14 @@
+angular.module('madLibs',[])
+	.controller('madCtrl', ['$scope', function($scope){
+		$scope.gender = 'male';
+		$scope.charName = '';
+		$scope.jobTitle = '';
+		$scope.tediousTask = '';
+		$scope.dirtyTask = '';
+		$scope.celebrity = '';
+		$scope.uselessSkill = '';
+		$scope.adjective = '';
+		$scope.obnoxiuousCelebrity = '';
+		$scope.hugeNumber = '';
+
+	}]);
